8431ce05ca1e64b0af954253f17f9553522749d9
Ali Haider <ShahidHaiderAlil@gmail.com>
2025-05-12 20:06:26 +0500

    Auto complete

--- receiving/urls.py
+++ receiving/urls.py
@@ -5,4 +5,5 @@ app_name = 'receiving'
 
 urlpatterns = [
     path('add_receivings/', views.add_receivings, name="add_receivings"),
+    path('customer-autocomplete/', views.customer_autocomplete, name='customer_autocomplete'),
 ]
--- receiving/views.py
+++ receiving/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 from .forms import ReceivingForm
 from .models import Receiving, Customer
 from django.views.generic.edit import CreateView
+from django.http import JsonResponse
 
 @login_required
 def add_receivings(request):
@@ -20,3 +21,11 @@ def add_receivings(request):
     else:
         form = ReceivingForm()
     return render(request, 'receiving/add_receiving.html', {'form': form})
+
+
+@login_required
+def customer_autocomplete(request):
+    query = request.GET.get('q', '')
+    customers = Customer.objects.filter(name__icontains=query)[:10]
+    results = [{'name': c.name, 'phone': c.phone} for c in customers]
+    return JsonResponse(results, safe=False)
